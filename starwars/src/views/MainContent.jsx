22ac9cd0cc18f828e59c4ef0c2be4909ef887fc5
Donald Whitely <dswhitely1@gmail.com>
2019-07-12 12:47:38 -0400

    MVP Complete

--- starwars/src/views/MainContent.jsx
+++ starwars/src/views/MainContent.jsx
@@ -1,37 +1,40 @@
-import React, {useEffect} from 'react';
-import {Segment, Container} from 'semantic-ui-react';
+import React from 'react';
+import {
+  Container,
+  Segment
+} from 'semantic-ui-react';
 
-import {useAxios} from '../hooks/useAxios';
+import { useAxios } from '../hooks/useAxios';
 
-const MainContent = ({characters}) => {
-  const [data, errors, loading] = useAxios('https://swapi.co/api/films/');
-    if (loading || data === []) {
-      return <h3>Loading</h3>
-    }
-    return (
+const MainContent = ( {characters} ) => {
+  const [data, errors, loading] = useAxios( 'https://swapi.co/api/films/' );
+  if (loading || data === []) {
+    return <h3>Loading</h3>;
+  }
+  return (
     <Container>
       <Segment.Group>
-        {characters &&  characters.data && characters.data.results.map(character => {
+        {characters && characters.data && characters.data.results.map( character => {
           return (
             <>
               <Segment>
-                <h1>{character.name}</h1>
-
+                              <h1>{character.name}</h1>
+  <span>{`  Born in year: ${character.birth_year}, With ${character.hair_color !== 'n/a' || character.hair_color !== 'none' ? character.hair_color : `no`} hair and ${character.eye_color} eyes`}</span>
               </Segment>
-            <Segment.Group>
-              {character.films.map(film => {
-                const filmId = data && data.data && data.data.results.filter(url => url.url === film)[0];
-                return (
-                  <Segment>{filmId && `${filmId.title}`}</Segment>
-                )
-              })}
-            </Segment.Group>
+              <Segment.Group>
+                {character.films.map( film => {
+                  const filmId = data && data.data && data.data.results.filter( url => url.url === film )[0];
+                  return (
+                    <Segment>{filmId && `${filmId.title}`}</Segment>
+                  );
+                } )}
+              </Segment.Group>
             </>
-          )
-        })}
+          );
+        } )}
       </Segment.Group>
     </Container>
-    )
-}
+  );
+};
 
 export default MainContent;
