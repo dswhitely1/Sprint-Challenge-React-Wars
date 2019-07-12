85e9379a618e58728130214edc88a893d89c21cf
Donald Whitely <dswhitely1@gmail.com>
2019-07-12 12:41:01 -0400

    Characters and MovieList being rendered to the screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react';
 import Navigation from './display/navigation/Navigation';
 
 import {useAxios} from './hooks/useAxios';
+import MainContent from './views/MainContent';
 
 const App = () => {
   const [data, errors, loading] = useAxios('https://swapi.co/api/people/');
@@ -14,8 +15,14 @@ const App = () => {
     setIsLoading(loading);
   },[loading]);
 
+  if (isLoading || data === []) {
+    return <h3>Loading</h3>
+  }
   return (
+    <>
     <Navigation characters={data}/>
+    <MainContent characters={data}/>
+    </>
   )
 };
 
--- starwars/src/display/navigation/Navigation.jsx
+++ starwars/src/display/navigation/Navigation.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {Menu, Container, Dropdown} from 'semantic-ui-react';
+import {
+  Container,
+  Dropdown,
+  Menu
+} from 'semantic-ui-react';
 
-const Navigation = ({characters}) =>  {
-  return (<Menu top inverted pointing size='large'>
+const Navigation = ( {characters} ) => {
+  return (<Menu inverted pointing size='large'>
     <Container>
-      <Menu.Item as='a' active>Digital Solutions by Don</Menu.Item>
+      <Menu.Item as='a'>Digital Solutions by Don</Menu.Item>
       <Menu.Item position='right'>
         <Dropdown item text='Characters'>
-        <Dropdown.Menu>
-          {characters && characters.data && characters.data.results.map(character=> <Dropdown.Item key={character.name}>{character.name}</Dropdown.Item>)}
-        </Dropdown.Menu>
+          <Dropdown.Menu>
+            {characters && characters.data && characters.data.results.map( character =>
+              <Dropdown.Item key={character.name}>{character.name}</Dropdown.Item> )}
+          </Dropdown.Menu>
         </Dropdown>
       </Menu.Item>
     </Container>
-  </Menu>)
-}
+  </Menu>);
+};
 
-export default Navigation
+export default Navigation;
