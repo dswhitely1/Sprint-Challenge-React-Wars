90a63c4962b4c13016f2410836fb4e8bef1404c6
Donald Whitely <dswhitely1@gmail.com>
2019-07-12 13:43:24 -0400

    Styling the Data

--- starwars/src/views/MainContent.jsx
+++ starwars/src/views/MainContent.jsx
@@ -3,7 +3,7 @@ import Loader from 'react-loader-spinner';
 import {
   Button,
   Container,
-  Segment
+  Segment, Header
 } from 'semantic-ui-react';
 import {ListingContainer} from '../styles/container';
 import { useAxios } from '../hooks/useAxios';
@@ -28,17 +28,16 @@ const MainContent = ( {characters} ) => {
         {characters && characters.data && characters.data.results.map( character => {
           return (
             <>
-              <Segment>
-                <h1>{character.name}</h1>
+              <Segment raised>
+                <Header as='h1'>{character.name}</Header>
                 <ListingContainer>
                 <span>{`  Born in year: ${character.birth_year}, With ${character.hair_color !== 'n/a' || character.hair_color !== 'none' ? character.hair_color : `no`} hair and ${character.eye_color} eyes.  More information, click the button to the right!  `}</span>
                 <Button primary
                         as="a"
-                        href={`https://www.starwars.com/databank/${kebabCase( character.name )}`}>More
-                  Information</Button>
+                        href={`https://www.starwars.com/databank/${kebabCase( character.name )}`}>{`More Info on ${character.name}`}</Button>
                 </ListingContainer>
               </Segment>
-              <Segment.Group>
+              <Segment.Group raised>
                 {character.films.map( ( film, i ) => {
                   const filmId = data && data.data && data.data.results.filter( url => url.url === film )[0];
                   return (
@@ -47,8 +46,7 @@ const MainContent = ( {characters} ) => {
                         {filmId && `${filmId.title}   `}
                         {filmId && <Button color="yellow"
                                            as="a"
-                                           href={`https://www.starwars.com/films/star-wars-episode-${romanNum( filmId.episode_id )}-${kebabCase( filmId.title )}`}>Click
-                          Here</Button>}
+                                           href={`https://www.starwars.com/films/star-wars-episode-${romanNum( filmId.episode_id )}-${kebabCase( filmId.title )}`}>{`Episode ${romanNum(filmId.episode_id).toUpperCase()}`}</Button>}
                       </ListingContainer>
                     </Segment>
                   );
